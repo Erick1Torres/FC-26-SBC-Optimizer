edd8b391034968e95c009c8499ec2986fb23669f
Erick1Torres <erickcacerestorres@gmail.com>
2026-04-26 18:59:59 +0200

    push

---
+++ api/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Asegúrate de que las rutas ahora apunten bien (un solo punto porque están al lado)
+const recipeRoutes = require('./routes/recipeRoutes');
+
+app.use(cors());
+app.use(express.json());
+
+// Esta ruta es para que TÚ compruebes si el server vive
+app.get('/api/v1/test', (req, res) => res.json({ status: "vivo" }));
+
+app.use('/api/v1/recipes', recipeRoutes);
+
+module.exports = app;
